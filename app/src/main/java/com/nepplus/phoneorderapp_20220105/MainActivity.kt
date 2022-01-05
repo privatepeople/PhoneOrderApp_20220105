a0b0d383619c1a1cf5e6956d7939e228cff1a22d
Lee Seung Hun <negrat@naver.com>
2022-01-05 16:00:05 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/phoneorderapp_20220105/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.phoneorderapp_20220105
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
